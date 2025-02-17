ed98d8956f53bb26c87e866df8e1d2e1ceaa4e48
JDQG <judaquego89@gmail.com>
2025-02-17 14:59:03 -0500

    GNURadio

--- Practica_1/GNU Radio/Practica_1.py
+++ Practica_1/GNU Radio/Practica_1.py
@@ -10,7 +10,7 @@
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio import blocks
+from gnuradio import analog
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
@@ -20,7 +20,7 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import Practica_1_epy_block_0 as epy_block_0  # embedded python block
+import Practica_1_epy_block_1 as epy_block_1  # embedded python block
 import sip
 
 
@@ -114,16 +114,16 @@ class Practica_1(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.epy_block_0 = epy_block_0.blk(additionFlag=1.0)
-        self.blocks_vector_source_x_0 = blocks.vector_source_f((1, 1, 1,  -1, -1, -1), True, 1, [])
+        self.epy_block_1 = epy_block_1.blk()
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_vector_source_x_0, 0), (self.epy_block_0, 0))
-        self.connect((self.blocks_vector_source_x_0, 0), (self.qtgui_time_sink_x_0, 1))
-        self.connect((self.epy_block_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.epy_block_1, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.epy_block_1, 0), (self.qtgui_time_sink_x_0, 1))
 
 
     def closeEvent(self, event):
@@ -139,6 +139,7 @@ class Practica_1(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
 
