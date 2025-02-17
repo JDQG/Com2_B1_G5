eecb3993e78c78ff8f9b4210bfebb56ff370f205
JDQG <judaquego89@gmail.com>
2025-02-17 15:12:37 -0500

    Diferenciador

--- Practica1/GNU Radio/Practica_1.py
+++ Practica1/GNU Radio/Practica_1.py
@@ -70,7 +70,7 @@ class Practica_1(gr.top_block, Qt.QWidget):
 
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
             1024, #size
-            samp_rate, #samp_rate
+            samp_rate/2, #samp_rate
             "", #name
             2, #number of inputs
             None # parent
@@ -117,7 +117,7 @@ class Practica_1(gr.top_block, Qt.QWidget):
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.epy_block_1 = epy_block_1.blk()
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_TRI_WAVE, 1000, 10, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f((samp_rate/2), analog.GR_SIN_WAVE, 1000, 10, 0, 0)
 
 
         ##################################################
@@ -141,8 +141,8 @@ class Practica_1(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq((self.samp_rate/2))
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/2)
 
 
 
