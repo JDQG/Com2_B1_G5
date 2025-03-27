34fcd6aca73ced3ad369d0df8b9e4f6b3cbf7ce1
JDQG <judaquego89@gmail.com>
2025-03-27 09:41:44 -0500

    flujograma bpsk

---
+++ Practica3/GNURadio/ook_epy_block_0_0.py
@@ -0,0 +1,22 @@
+import numpy as np
+from gnuradio import gr
+import math
+
+class blk(gr.sync_block):  
+    """This block is a CE VCO or baseband VCO and works as following: Recibe dos entradas: una que controla la amplitud de la señal y otra que ajusta la fase, y produce una salida compleja donde la parte real e imaginaria representan la modulación en cuadratura. La señal resultante se calcula como una exponencial compleja, lo que permite crear modulaciones digitales o generar señales en banda base para comunicaciones."""
+
+    def __init__(self,):  
+        gr.sync_block.__init__(
+            self,
+            name='e_CE_VCO_fc',   
+            in_sig=[np.float32, np.float32],
+            out_sig=[np.complex64]
+        )
+        
+    def work(self, input_items, output_items):
+        A=input_items[0]
+        Q=input_items[1]
+        y=output_items[0]
+        N=len(A)
+        y[:]=A*np.exp(1j*Q)
+        return len(output_items[0])
